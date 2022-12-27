b3425d8b67841cf519a7669772858780ce9e74e9
togrul-asgerli <81424132+togrul-asgerli@users.noreply.github.com>
2022-12-27 10:38:08 +0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using MVC_Empty.Models;
+using MVC_Empty.Models.EntityFrameWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using System.Web.Routing;
+using System.Web.UI.WebControls.WebParts;
+
+namespace MVC_Empty.Views
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        NorthwindEntities db=new NorthwindEntities();
+        public ActionResult Index()
+        {
+            var model = db.Orders.ToList();
+            return View(model);
+        }
+
+        [Route("Home/GetId")]
+        public ActionResult GetId()
+        {
+            return Content("Hahaha");
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Insert()
+        {
+            return View();
+        }
+        [HttpPost]
+        
+        public ActionResult Insert(Order ord)
+        {
+            if(ord.OrderID==0)
+            {
+                db.Orders.Add(ord);
+            }
+            else
+            {
+                var updateData = db.Orders.Find(ord.OrderID);
+                if(updateData==null)
+                {
+                    return HttpNotFound();
+                }
+                updateData.ShipName=ord.ShipName;
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult Update(int id)
+        {
+            var model = db.Orders.Find(id);
+            if(model==null)
+            {
+                return HttpNotFound();
+            }
+            return View("Insert", model) ;
+        }
+        public ActionResult Delete(int id)
+        {
+            var deleteOrder=db.Orders.Find(id);
+            if(deleteOrder==null)
+            {
+                return HttpNotFound();
+            }
+            db.Orders.Remove(deleteOrder);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/Palacs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Empty.Models
+{
+    public class Palacs
+    {
+        public int Id { get; set; }
+        public string Name{ get; set; }
+        public List<Palacs> Person { get; set; }
+    }
+}
